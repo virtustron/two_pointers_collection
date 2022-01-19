340441fe85db63b91bd0324385e95b6dab9c2ce6
Vitalii Tron <virtustron@gmail.com>
2022-01-19 14:28:18 +0200

    Add twp `length` fields

--- src/two_head_vec.rs
+++ src/two_head_vec.rs
@@ -1,79 +1,74 @@
+use std::sync::{Arc, Mutex};
 use std::sync::atomic::AtomicPtr;
 
+use core::mem::MaybeUninit;
 
-use std::sync::Arc;
-use std::sync::Mutex;
 
 struct TwoHeadVec<T> {
-    head_read: AtomicPtr,            // -> buf1
-    head_write: Mutex<usize>,        // -> buf2
+    head_read: AtomicPtr,                 // -> buf1
+    head_write: Arc<Mutex<usize>>,        // -> buf2
 
-    buffer_1: *mut T,             
-    buffer_2: *mut T,             
+    buffer_read: *mut T,             
+    buffer_write: *mut T,             
     
-    capacity_1: usize,            
-    capacity_2: usize,    
+    capacity_read: usize,            
+    capacity_write: usize,    
+
+    length_read: usize,            
+    length_write: usize,  
     
     // aproach 1 generation
-    generation: CachePadded<AtomicUsize>,     // `CachePadded` - to avoid cache locality.   
+    generation: CachePadded<AtomicUsize>,     // `CachePadded` - to avoid cache locality.  
+    
+    _marker: PhantomData<T>,
 }
 
-unsafe impl<T: Send> Sync for TwoHeadVec<T> {}
 unsafe impl<T: Send> Send for TwoHeadVec<T> {}
+unsafe impl<T: Sync> Sync for TwoHeadVec<T> {}
 
 impl<T> TwoHeadVec<T> {
     pub fn new(capacity: usize) -> TwoHeadVec<T> {
         assert!(capacity > 0, "capacity must be non-zero");
 
         head_read = AtomicPtr::default();
-        head_write = Mutex::new(0);
+        head_write = Arc::new(Mutex::new(0));
 
-        let buffer_1 = vec![0; capacity];
-        let buffer_1 = buffer_1.into_boxed_slice();
+        let buffer_read = vec![0; capacity];
+        let buffer_read = buffer_read.into_boxed_slice();
 
-        let buffer_2 = vec![0; capacity];
-        let buffer_2 = buffer_2.into_boxed_slice();
+        let buffer_write = vec![0; capacity];
+        let buffer_write = buffer_write.into_boxed_slice();
 
-        capacity_1 = capacity;
-        capacity_2 = capacity;
+        capacity_read = capacity;
+        capacity_write = capacity;
+
+        length_read = 0;
+        length_write = 0;
 
         generation = 0;
 
         TwoHeadVec {
             head_read,
             head_write,
-            buffer_1,
-            buffer_2,
-            capacity_1,
-            capacity_2,
+            buffer_read,
+            buffer_write,
+            capacity_read,
+            capacity_write,
+            length_read,
+            length_write,
             generation,
+            _marker: PhantomData,
         }
     }
 
-
-
-    
-
-}
-
-new() {
-
-    /*
-    // approach 1
-    
-    generation = 0
-    */
-}
-
-
-push() {
+    pub fn push(&self, index: usize, value: T) -> Result<(), T> {
 
     /*
     // approach 1
 
     Mutex  head_write lock() {
         
-        1. modify `head_write`
+        
 
         2. swap `head_read` and `head_write` - atomic operation
           2.1 store head_read   p = head_read.load()
@@ -86,10 +81,30 @@ push() {
 
     }  
     */
+        assert!(index >= 0, "index must be equal or greater than zero");
+        assert!(index < capacity_write, "index must be greater than zero");
+
+        // 1. modify `head_write`
+        self.head_write.lock().unwrap();
+
+        length_write
+        
+        let slot = unsafe { &*self.buffer_write.add(index) };         // shift pointer to `index`
+        
+        unsafe {
+            slot.value.get().write(MaybeUninit::new(value));
+        }
+
+
+
+        
+    }
+
+    
 
 }
 
-
+/*
 get() {
     /*
     // approach 1
@@ -113,5 +128,5 @@ get() {
 
 }
 
-
+*/
 
