fc38a030b44f40d4ea15cda1e5a1c631e880730d
Vitalii Tron <virtustron@gmail.com>
2022-01-20 11:25:21 +0200

    Fix pointer isues in `new`

--- src/test.rs
+++ src/test.rs
@@ -1,15 +1,10 @@
-mod two_head_vec;
-
 use crate::two_head_vec::TwoHeadVec;
 
-fn main() {
-    println!("Hello, world!");
 
+#[test]
+fn push_two_elements_succesful() {
     let v = TwoHeadVec::new(2);
 
     assert_eq!(v.push('a'), Ok(()));
     assert_eq!(v.push('b'), Ok(()));
 }
-
-#[cfg(test)]
-mod test;
--- src/two_head_vec.rs
+++ src/two_head_vec.rs
@@ -2,13 +2,19 @@ use std::ptr;
 
 use std::sync::{Arc, Mutex};
 use std::sync::atomic::AtomicPtr;
+use std::marker::PhantomData;
 
 use core::mem::MaybeUninit;
 
+use core::sync::atomic::{self, AtomicUsize, Ordering};
 
-struct TwoHeadVec<T> {
-    head_read: AtomicPtr,                 // -> buf1
-    head_write: Arc<Mutex<usize>>,        // -> buf2
+use crossbeam_utils::CachePadded;
+
+
+
+pub struct TwoHeadVec<T> {
+    head_read: AtomicPtr<T>,               // -> buf1    - the same in-memory representation as a `*mut T`
+    head_write: Arc<Mutex<*mut T>>,        // -> buf2
 
     buffer_1: *mut T,             
     buffer_2: *mut T,             
@@ -31,67 +37,67 @@ impl<T> TwoHeadVec<T> {
     pub fn new(capacity: usize) -> TwoHeadVec<T> {
         assert!(capacity > 0, "capacity must be non-zero");
 
-        capacity.store(capacity, Ordering::Relaxed);
-
-        length_read.store(0, Ordering::Relaxed);
-        let buffer_1 = vec![0; capacity].into_boxed_slice();
-        head_read = AtomicPtr::from(buffer_1);
+        let boxed_buffer_1 = vec![0; capacity].into_boxed_slice();
+        let buffer_1 = Box::into_raw(boxed_buffer_1) as *mut T;
+        let head_read = AtomicPtr::new(buffer_1);
         
-        length_write.store(0, Ordering::Relaxed);
-        let buffer_2 = vec![0; capacity].into_boxed_slice();
-        head_write = Arc::new(Mutex::new(buffer_2));
+        let boxed_buffer_2 = vec![0; capacity].into_boxed_slice();
+        let buffer_2 = Box::into_raw(boxed_buffer_2) as *mut T;
+        let head_write = Arc::new(Mutex::new(buffer_2));
 
-        generation = 0;   // other approach - do count of the references to `head_read`
+        let generation: usize = 0;   // other approach - do count of the references to `head_read`
 
         TwoHeadVec {
-            head_read,
+            head_read, 
             head_write,
             buffer_1,
             buffer_2,
-            capacity,
-            length_read,
-            length_write,
-            generation,
+            capacity: CachePadded::new(AtomicUsize::new(capacity)),
+            length_read: CachePadded::new(AtomicUsize::new(0)),
+            length_write: CachePadded::new(AtomicUsize::new(0)),
+            generation: CachePadded::new(AtomicUsize::new(0)),
             _marker: PhantomData,
         }
     }
 
     pub fn push(&self, value: T) -> Result<(), T> {
-        assert!(length_write + 1 < capacity, "vector is already full");
+        assert!(self.length_write + 1 < self.capacity, "vector is already full");
 
         self.head_write.lock().unwrap();
         
         // 1. modify `head_write`
-        length_write += 1;
-        let slot = unsafe { &*self.buffer_2.add(length_write - 1) };
+        self.length_write.fetch_add(1, Ordering::SeqCst);
+        let slot = unsafe { &*self.buffer_2.add(self.length_write - 1) };
         unsafe { slot.value.get().write(MaybeUninit::new(value)); }
         
         // 2. swap `head_read` and `head_write` and `capacity`, `length` respectively
-        let temp_head = head_read.load(Ordering::Acquire);        
-        head_read.store(self.head_write, Ordering::Release);     
-        generation.fetch_add(1, Ordering::SeqCst);
-        head_write = temp_head; 
+        let temp_head = self.head_read.load(Ordering::Acquire);        
+        self.head_read.store(self.head_write, Ordering::Release);     
+        self.generation.fetch_add(1, Ordering::SeqCst);
+        self.head_write = temp_head; 
               
-        let temp_length = length_read.load(Ordering::Acquire);    
-        length_read.store(self.length_write, Ordering::Release);  
-        length_write = temp_length; 
+        let temp_length = self.length_read.load(Ordering::Acquire);    
+        self.length_read.store(self.length_write, Ordering::Release);  
+        self.length_write = temp_length; 
                 
         //3. deep copy (content copying) `head_read` -> `head_write`
-        ptr::copy(head_read, head_write, capacity.load(Ordering::Acquire));
+        ptr::copy(self.head_read, self.head_write, self.capacity.load(Ordering::Acquire));
+
+        return self.length_write.load(Ordering::Acquire)
     }  
 
     pub fn get(&self, index: usize) -> Result<T, ()> {
         // TODO let backoff = Backoff::new();
 
         assert!(index > 0, "index must be greater than zero");
-        assert!(index < length_read.load(Ordering::Acquire), "index must be less than length");
+        assert!(index < self.length_read.load(Ordering::Acquire), "index must be less than length");
 
         loop {
-            let current_generation = generation.load(Ordering::SeqCst);
+            let current_generation = self.generation.load(Ordering::SeqCst);
 
             let value = unsafe { &*self.head_read.load(Ordering::SeqCst).fetch_add(index) };
 
-            if current_generation == generation.load(Ordering::SeqCst) {
+            if current_generation == self.generation.load(Ordering::SeqCst) {
                 return Some(value)
             }
         }
