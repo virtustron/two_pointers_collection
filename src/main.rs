96a78840bd70eaff620a83573e5aad855b5d6f97
Vitalii Tron <virtustron@gmail.com>
2022-01-20 15:50:12 +0200

    Fix single-thread `push` and `get` issues

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,13 @@ use crate::two_head_vec::TwoHeadVec;
 fn main() {
     println!("Hello, world!");
 
-    let v = TwoHeadVec::new(2);
+    let v = TwoHeaFdVec::new(2);
 
     assert_eq!(v.push('a'), Ok(()));
     assert_eq!(v.push('b'), Ok(()));
+
+    assert_eq!(v.get(0), Ok('a'));
+    assert_eq!(v.get(1), Ok('b'));
 }
 
 #[cfg(test)]
